69bdded6fd4ca0bf4925c298113c2bde25831b86
Fabio Giannace <fabio.giannace@gmail.com>
2022-09-01 22:41:52 +0200

    .

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
 	const expenses = [
 		{
 			id: 'e1',
@@ -31,9 +31,9 @@ function App() {
 	return (
 		<div>
 			<h2>Let's get started!</h2>
-			<Expenses items={expenses[1]} />
+			<Expenses items={expenses} />
 		</div>
 	);
-}
+};
 
 export default App;
